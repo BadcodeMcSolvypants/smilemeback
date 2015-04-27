a94f390b166ef9f3099369e84655ba25c7d010cb
Timo Petmanson <tpetmanson@gmail.com>
2015-04-28 01:45:41 +0300

    Better GNU license header

--- app/src/main/java/com/smilemeback/storage/Category.java
+++ app/src/main/java/com/smilemeback/storage/Category.java
@@ -1,5 +1,5 @@
-/**
- * This file is part of SmileMeBack.
+/*
+ This file is part of SmileMeBack.
 
  SmileMeBack is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
--- app/src/main/java/com/smilemeback/storage/datamover/DataMover.java
+++ app/src/main/java/com/smilemeback/storage/datamover/DataMover.java
@@ -1,5 +1,5 @@
-/**
- * This file is part of SmileMeBack.
+/*
+ This file is part of SmileMeBack.
 
  SmileMeBack is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  You should have received a copy of the GNU General Public License
  along with SmileMeBack.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.smilemeback.misc;
+package com.smilemeback.storage.datamover;
 
 import com.google.common.collect.Ordering;
 
